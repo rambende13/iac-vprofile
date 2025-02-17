025a8f9d9ad7542a7d5d75fbb904420b7180dee8
devops4sure <devops4sure@gmail.com>
2025-02-17 15:18:45 +0530

    Bucket name & cluster name updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -30,6 +30,7 @@ terraform {
     bucket = "gitopsterrastate"
     key    = "terraform.tfstate"
     region = "us-east-2"
+  
   }
 
   required_version = "~> 1.6.3"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,4 +8,5 @@ variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
   default     = "kitops-eks"
+
 }
